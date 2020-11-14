b826314a565f0a44602895efb3f044a6d2e74d20
vldmr17 <svma365@gmail.com>
2020-11-14 16:29:25 +0300

    Initial Commit

---
+++ Groundbook/GroundbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroundbookApp.swift
+//  Groundbook
+//
+//  Created by admin on 14.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct GroundbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
